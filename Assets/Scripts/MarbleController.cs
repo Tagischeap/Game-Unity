585043622cbc4e90b4dea13042d89b5e23566bef
Tagischeap <Tagischeap@Gmail.com>
2022-02-02 21:24:13 -0800

    More improvement on vehicle controller
    Made the ball smaller.
    Made the ball behave less floaty.
    Only accelerates when touching the ground.
    Added a Hop function. (Planning it to proceed to drift when held)
    Added a Boost function. (Right now it only increases the max speed)
    
    Did a little mapping to make it look nicer, and to practice some technique.

--- Assets/Scripts/MarbleController.cs
+++ Assets/Scripts/MarbleController.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class MarbleController : MonoBehaviour
 {
-    public Rigidbody sphereRB;
-    //public Rigidbody sphereRB;
+    [SerializeField] private Rigidbody sphereRB;
+    [SerializeField] private SphereCollider sphereCL; 
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
     private float horizontalInput;
     private float verticalInput;
+    private bool jumpInput;
+    private bool sprintInput;
     private float currentForce;
     public float maxForce;
     public float forwardForce;
     public float steerForce;
+    public float jumpForce;
+    public float maxBoost;
+    private float boostForce;
+    public bool drifting;
+    public bool boosting;
+
+    public bool frontRWheel;
+    public bool frontLWheel;
+    public bool rearRWheel;
+    public bool rearLWheel;
+
+    [SerializeField] private Transform fRWheelOffset;
+    [SerializeField] private Transform fLWheelOffset;
+    [SerializeField] private Transform rRWheelOffset;
+    [SerializeField] private Transform rLWheelOffset;
+
     // Start is called before the first frame update
     private void Start() 
     {
@@ -23,18 +41,47 @@ public class MarbleController : MonoBehaviour
     private void Update()
     {
         GetInput();
+        GetRayCasts();
     }
     private void FixedUpdate() 
     {
-        Debug.Log(maxForce + " : " + currentForce + " | " + forwardForce + ", " + verticalInput);
+        //Debug.Log(maxForce + " : " + currentForce + " | " + forwardForce + ", " + verticalInput);
         // set model position to sphere
         transform.position = sphereRB.transform.position;
         //adjust speed for sphere
         currentForce = verticalInput;
-        currentForce *= verticalInput != 0 ? forwardForce : maxForce;
-        if (verticalInput != 0)
+        currentForce *= (verticalInput != 0 ? forwardForce : maxForce) + boostForce;
+        if (frontLWheel || frontRWheel || rearLWheel || rearRWheel)
         {
-            sphereRB.AddForce(transform.forward * currentForce, ForceMode.Acceleration);
+            if (verticalInput != 0)
+            {
+                sphereRB.AddForce(transform.forward * currentForce, ForceMode.Acceleration);
+            }
+            if (jumpInput)
+            {
+                if(drifting)
+                {
+
+                }
+                else
+                {
+                    sphereRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+                    drifting = true;
+                }
+            }
+            else
+            {    
+                drifting = false;
+            }
+        }
+
+        if (sprintInput)
+        {
+            boostForce = maxBoost;
+        }
+        else
+        {
+            boostForce = 0;
         }
         //steer the sphere
         float rot = horizontalInput * steerForce * Time.deltaTime;
@@ -45,5 +92,23 @@ public class MarbleController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis(HORIZONTAL);
         verticalInput = Input.GetAxis(VERTICAL);
+        jumpInput = Input.GetButton("Jump");
+        sprintInput = Input.GetButton("Fire3");
+        
+
+    }
+
+    private void GetRayCasts()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(fRWheelOffset.position, Vector3.down, frontRWheel ? Color.blue :Color.green);
+        Debug.DrawRay(fLWheelOffset.position, Vector3.down, frontLWheel ? Color.blue :Color.green);
+        Debug.DrawRay(rRWheelOffset.position, Vector3.down, rearRWheel ? Color.blue :Color.green);
+        Debug.DrawRay(rLWheelOffset.position, Vector3.down, rearLWheel ? Color.blue :Color.green);
+
+        frontRWheel = Physics.Raycast(fRWheelOffset.position, Vector3.down, out hit, 1.0f);
+        frontLWheel = Physics.Raycast(fLWheelOffset.position, Vector3.down, out hit, 1.0f);
+        rearRWheel = Physics.Raycast(rRWheelOffset.position, Vector3.down, out hit, 1.0f);
+        rearLWheel = Physics.Raycast(rLWheelOffset.position, Vector3.down, out hit, 1.0f);
     }
 }
